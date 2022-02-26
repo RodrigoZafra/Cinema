fb159cb45780231e763bb54a519b50cd268a7cc6
RodrigoZafra <rodrifdt17@gmail.com>
2022-02-26 14:08:28 +0100

    haciendo formulario para comprar las entradas

--- app/src/main/java/com/rodrigo/cinema/FormularioActivity.java
+++ app/src/main/java/com/rodrigo/cinema/FormularioActivity.java
@@ -3,13 +3,18 @@ package com.rodrigo.cinema;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class FormularioActivity extends AppCompatActivity {
     private Spinner spinnerCine;
+    private EditText etNumEntradas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,7 @@ public class FormularioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
 
         spinnerCine = findViewById(R.id.spinnerCine);
+        etNumEntradas = findViewById(R.id.editTextNumber);
 
         ArrayList<String> cines = new ArrayList<>();
         cines.add("Rivas H2O");
@@ -28,4 +34,11 @@ public class FormularioActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, cines);
         spinnerCine.setAdapter(adapter);
     }
+
+    public void elegirAsiento(View view) {
+        ImageView iv = findViewById(view.getId());
+        iv.setImageDrawable(getDrawable(R.drawable.asientoocupado));
+    }
+
 }
+
